61ef11bba6eaddc47a3ea66e722fb5aea4a7e745
Pangsu <wxzgks@gmail.com>
2019-06-21 14:53:52 +0900

    Step5_1 ListView 안에 Spinner를 넣으면 클릭되게 수정 하였음. 하지만 클릭한 데이터가 사라지게 못해서 수정하는 중

--- app/src/main/java/com/example/emeetingwhat/Step5_1_SelectTextActivity.java
+++ app/src/main/java/com/example/emeetingwhat/Step5_1_SelectTextActivity.java
@@ -1,15 +1,12 @@
 package com.example.emeetingwhat;
 
 import android.content.Context;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.BaseAdapter;
-import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -19,9 +16,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import java.util.ArrayList;
 
 public class Step5_1_SelectTextActivity extends AppCompatActivity {
-    ArrayList<String> mTempName;    //가상 데이타 베이스
-    ArrayList<MyItem> mArMember;
-    int mPeopleMax;
+    ArrayList<String> mDatabase;    //가상 데이터 베이스
+    ArrayList<MyItem> mMyItemList;    //가상 데이터 베이스
+    NameTable mNameTable;
+    int mPeopleMax; //가상 데이터베이스 개체 수
     ListView mListMember;
     MyListAdapter MyAdapter = null;
     ArrayAdapter<String> arrayAdapter;
@@ -32,11 +30,47 @@ public class Step5_1_SelectTextActivity extends AppCompatActivity {
 
         initNameList();
 
-        arrayAdapter = new ArrayAdapter<>(this,android.R.layout.simple_spinner_item, mTempName);
         initListView();
         // ListView 위젯에 10개의 텍스트 항목을 추가
+        refreshList();
 
     }
+    public class NameTable {
+        String[] mNameList;
+        boolean[] isUsedList;
+        NameTable(){
+            isUsedList = new boolean[mPeopleMax];
+        }
+        public void refreshList(){
+            if(mNameList != null)
+                mNameList = null;
+            mNameList = new String[notUsedCount()+1];
+            int index = 0;
+            mNameList[index++] = "";
+            for(int i = 0 ; i < mPeopleMax; i ++) {
+                if(isUsedList[i] == false)
+                    mNameList[index++] = mDatabase.get(i);
+            }
+        }
+        public int notUsedCount(){
+            int count = 0;
+            for(int i = 0 ; i < mPeopleMax; i ++){
+                if(isUsedList[i] == false)
+                    count++;
+            }
+            return  count;
+        }
+        public void useName(int index)
+        {
+            isUsedList[index] = true;
+        }
+    }
+
+    public class MyItem{
+        Spinner spinner;
+    }
+
+
 
     public void onClick(View v)
     {
@@ -59,98 +93,85 @@ public class Step5_1_SelectTextActivity extends AppCompatActivity {
     }
     // ListView 를 초기화
     public void initNameList() {
-        if (mTempName != null) {
-            mTempName.clear();
-            mTempName = null;
+        if (mDatabase != null) {
+            mDatabase.clear();
+            mDatabase = null;
         }
-        mTempName = new ArrayList<>();
-        mTempName.add("은지");
-        mTempName.add("수미");
-        mTempName.add("창수");
+        mDatabase = new ArrayList<>();
+        mDatabase.add("은지");
+        mDatabase.add("수미");
+        mDatabase.add("팡수");
         for (int i = 0; i < 1; i++) {
-            mTempName.add("태엽" + (i + 1));
+            mDatabase.add("태엽" + (i + 1));
         }
-        mPeopleMax = mTempName.size();
+        mPeopleMax = mDatabase.size();
+        /////가상 데이터 세팅//////
+
+        mNameTable = new NameTable();
+        mNameTable.refreshList();
+
+        mMyItemList = new ArrayList<>();
+        for (int i = 0; i < mPeopleMax; i++) {
+            mMyItemList.add(new MyItem());
+        }
+        arrayAdapter = new ArrayAdapter<>(this,android.R.layout.simple_spinner_item, mNameTable.mNameList);
     }
 
     public void initListView() {
-        if( mArMember != null ) {
-            mArMember.clear();
-            mArMember = null;
-        }
-        // ArrayList 배열 객체를 생성
-        mArMember = new ArrayList<>();
-        MyItem mi;
-        for(int i=0; i < mPeopleMax; i++) {
-            mi = new MyItem();
-            mArMember.add(mi);
-        }
 
         if( MyAdapter != null ) MyAdapter = null;
         // 어댑터 객체를 생성해서 ListView 에 지정
-        MyAdapter = new MyListAdapter(this, R.layout.custom_list_item, mArMember);
+        MyAdapter = new MyListAdapter(this, R.layout.custom_list_item, mMyItemList);
 
         // ListView 위젯의 핸들을 구해서 멤버변수에 저장
         mListMember = findViewById(R.id.listMember);
         // ListView 의 어댑터를 지정
         mListMember.setAdapter(MyAdapter);
     }
-    public class MyItem {
-        View mLayoutItem = null;
-    }
 
     // ListView 와 데이터 배열을 연결해주는 커스텀 어댑터 클래스를 정의
-    public class MyListAdapter extends BaseAdapter {
-        LayoutInflater mInflater;
-        ArrayList<MyItem> mArSrc;
+    public class MyListAdapter extends ArrayAdapter<MyItem> {
+        //LayoutInflater mInflater;
         int layout;
+        Context mContext;
+        ArrayList<MyItem> arSrc;
 
         // 생성자 함수에서 멤버변수 초기화
         MyListAdapter(Context context, int aLayout, ArrayList<MyItem> aarSrc) {
-            mInflater = LayoutInflater.from(context);
-                    /*(LayoutInflater) context.getSystemService(
-                    Context.LAYOUT_INFLATER_SERVICE);*/
-            mArSrc = aarSrc;
+            super(context,aLayout,aarSrc);
+            //mInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             layout = aLayout;
+            mContext = context;
+            arSrc = aarSrc;
         }
 
-        // 항목 개수를 반환
-        public int getCount() {
-            return mArSrc.size();
-        }
+        public View getView(final int position, View convertView, ViewGroup parent) {
+            View row = convertView;
+            if(row == null){
+                LayoutInflater inflater = getLayoutInflater();
+                row = inflater.inflate(layout, parent, false);
+            }
 
-        // 특정 항목의 텍스트 데이터를 반환
-        public View getItem(int position) {
-            return mArSrc.get(position).mLayoutItem;
-        }
+            // 문자열을 구한다
+            TextView textView1 = row.findViewById(R.id.text1);
+            textView1.setText(position + 1 + "번");
 
-        // 특정 항목의 ID 를 반환
-        public long getItemId(int position) {
-            return position;
-        }
-
-        // ListView 아이템 내부 각각의 엘리먼트에 데이터를 입력
-        public View getView(int position, View convertView, ViewGroup parent) {
-
-            // 데이터 배열에서 해당 항목을 구한다
-            MyItem mi = mArSrc.get(position);
-            // 항목 Layout 이 아직 생성되지 않았다면 생성한다
-            if( mi.mLayoutItem == null )
-                mi.mLayoutItem = mInflater.inflate(layout, null);
-
-            // 1번째 TextView 에 데이터 입력
-            TextView textView = mi.mLayoutItem.findViewById(R.id.text1);
-            textView.setText(position+1 + "번");
-
-
-            Spinner spinner =  mi.mLayoutItem.findViewById(R.id.spnFriend);
-            ///////// ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(getApplicationContext(),android.R.layout.simple_spinner_item, mTempName);
-            spinner.setAdapter(arrayAdapter);
-            /*
-            spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            MyItem mi = arSrc.get(position);
+            // EditText 에 임시 데이터 입력
+            mi.spinner = row.findViewById(R.id.spnFriend);
+            mi.spinner.setAdapter(arrayAdapter);
+            // TODO : 이전 i 값 저장 변수 만들기
+            mi.spinner.getSelectedItem();
+            mi.spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
                 @Override
                 public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                    //Toast.makeText(getApplicationContext(), i + "가 선택되었습니다.",Toast.LENGTH_SHORT).show();
+                    if(i>0) {
+                        Toast.makeText(getApplicationContext(),i + "가 선택되었습니다.",Toast.LENGTH_SHORT).show();
+                        //String str = arSrc.get(position).spinner.getSelectedItem().toString();
+                        //mNameTable.useName(i);
+                        //mNameTable.refreshList();
+
+                    }
                 }
 
                 @Override
@@ -159,9 +180,12 @@ public class Step5_1_SelectTextActivity extends AppCompatActivity {
                 }
 
             });
-            */
-            return mi.mLayoutItem;
+            return row;
         }
     }
 
+    public void refreshList() {
+        MyListAdapter MyAdapter = (MyListAdapter)mListMember.getAdapter();
+        MyAdapter.notifyDataSetChanged();
+    }
 }
